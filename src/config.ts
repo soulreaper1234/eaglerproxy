ef008e102943b32d11ee336ebc1110cf2925a6b4
soulreaper1234 <124217453+soulreaper1234@users.noreply.github.com>
2023-10-27 21:59:20 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -27,8 +27,8 @@ export const config: Config = {
       originBlacklist: null,
     },
     server: {
-      host: "no",
-      port: 46625,
+      host: "play.thanatos-network.xyz",
+      port: 4297,
     },
     tls: undefined,
   },
